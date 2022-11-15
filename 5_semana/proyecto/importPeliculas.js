242dc0f8a3c6bd1d97090f73f90ae02acc7457a6
Felipe Escobar R <pipesco93@gmail.com>
2022-11-14 21:13:14 -0500

    Se completa el ejercicio 1 de la ejercitacion, se agrega el archivo se agregsa el archivo import peliculas

---
+++ 5_semana/proyecto/importPeliculas.js
@@ -0,0 +1,12 @@
+let listaPelis = require('./peliculas');
+
+for (i in listaPelis){
+    for (const [key, value] of  Object.entries(listaPelis[i])){
+        console.log(`${key}: ${value}`)
+    }
+    console.log('\n\n')
+}
+
+// otra forma
+//listaPelis.forEach(i => console.log(i));
+
