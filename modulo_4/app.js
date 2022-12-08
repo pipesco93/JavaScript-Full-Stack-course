8d5e4cf2eddc83a2e232ba104e9fa80abb4cf849
Felipe Escobar R <pipesco93@gmail.com>
2022-12-07 21:30:50 -0500

    actualizacions

--- modulo_4/app.js
+++ modulo_4/app.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
  res.send('¡Hola mundo!');
 });
 
+app.get('/html', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/index.html'));
+   });
+
 app.listen(3001, () =>
  console.log('Servidor corriendo')
 );
