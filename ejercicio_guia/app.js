783764938504db37e8d0c81104054f1a72df7b25
Felipe Escobar R <pipesco93@gmail.com>
2023-01-09 18:56:35 -0500

    Se agrega estructura a mercado liebre, se agrega registro y log in

--- ejercicio_guia/app.js
+++ ejercicio_guia/app.js
@@ -13,8 +13,12 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './views/home.html'));
 });
 
-app.get('/about', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/about.html'));
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/register.html'));
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/login.html'));
 });
 
 
