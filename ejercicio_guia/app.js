8a6687d1db84fc45400e545db58e54b373c8be41
Felipe Escobar R <pipesco93@gmail.com>
2023-01-09 20:07:27 -0500

    Se agrega codigo para poder hacer el deply en render

--- ejercicio_guia/app.js
+++ ejercicio_guia/app.js
@@ -2,12 +2,14 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+const port = process.env.PORT || 3001; //para el deploy
+
 
 const publicPath = path.resolve(__dirname, "./public");
 
 app.use(express.static(publicPath));
 
-app.listen(3001, () => console.log('Servidor corriendo'));
+app.listen(3001, () => console.log(`Servidor corriendo ${port}`));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './views/home.html'));
