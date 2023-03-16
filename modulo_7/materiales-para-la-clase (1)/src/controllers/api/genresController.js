74a70d11fc697ac968e077413f23db16e1028d98
Felipe Escobar R <pipesco93@gmail.com>
2023-03-15 19:01:02 -0500

    Se agrega la primera ejercitacion del modulo 7 de API resuleta hasta el segundo punto

---
+++ modulo_7/materiales-para-la-clase (1)/src/controllers/api/genresController.js
@@ -0,0 +1,31 @@
+const db = require('../../database/models');
+const sequelize = db.sequelize;
+
+module.exports = {
+    'list': (req, res) => {
+        db.Genre
+            .findAll()
+            .then(genres => {
+                return res.status(200).json({
+                    meta: {
+                        status: 200,
+                        totatl: genres.length,
+                        url: "api/genres"
+                    },
+                    data: genres})
+            })
+    },
+    'detail': (req, res) => {
+        db.Genre
+            .findByPk(req.params.id)
+            .then(genre => {
+                return res.status(200).json({
+                    data: genre,
+                    satus:200,
+                    url: `api/genres/`+req.params.id
+                });
+            });
+    }
+};
+
+
