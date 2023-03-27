084f442a27a3c5908735993ca2668a5471cdedbf
Felipe Escobar R <pipesco93@gmail.com>
2023-03-27 18:48:55 -0500

    Update config.js

--- modulo_7/materiales-para-la-clase (1)/src/database/config/config.js
+++ modulo_7/materiales-para-la-clase (1)/src/database/config/config.js
@@ -16,7 +16,7 @@ module.exports = {
   // },
   "development": {
     "username": "root",
-    "password": "pipesco93",
+    "password": "",
     "database": "movies_db",
     "host": "127.0.0.1",
     "dialect": "mysql"
