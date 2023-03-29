7897695f947068c239b029c9fa5380a82bc4f642
Felipe Escobar R <pipesco93@gmail.com>
2023-03-29 18:48:36 -0500

    se agrega archivo .env en modulo 8 para manejo de contrasenas

--- modulo_8/src/database/config/config.js
+++ modulo_8/src/database/config/config.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
+
 module.exports = {
   "development": {
     "username": "root",
-    "password": '',
+    "password": process.env.PASSWORD,
     "database": "movies_db",
     "host": "127.0.0.1",
     "dialect": "mysql"
